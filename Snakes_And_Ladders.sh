5ebe2662fdaeb59e23f4f5255f7676429a177a42
vegirevathi <vegirevathi@gmail.com>
2020-06-10 22:58:44 +0530

    Added: Winning condition

--- Snakes_And_Ladders.sh
+++ Snakes_And_Ladders.sh
@@ -8,22 +8,32 @@ echo $dieValue
 
 function checkOptions()
 {
-	option=$(( RANDOM%3 + 1 ))
 	playerPosition=$START_POSITION	
 	noPlay=1
 	ladder=2
 	snake=3
-	case $option in
-	$noPlay)
-		playerPosition=$playerPosition
-		;;
-	$ladder)
-		playerPosition=$(( $playerPosition + $dieValue ))
-		;;
-	$snake)
-		playerPosition=$(( $playerPosition - $dieValue ))
-		;;
-	esac
+	while [ $playerPosition -le 100 ]
+	do
+		option=$(( RANDOM%3 + 1 ))
+		case $option in
+		$noPlay)
+			playerPosition=$playerPosition
+			;;
+		$ladder)
+			playerPosition=$(( $playerPosition + $dieValue ))
+			;;
+		$snake)
+			if [ $(( $playerPosition - $dieValue)) -le $START_POSITION ]
+      	then
+         	playerPosition=$START_POSITION
+			else
+				playerPosition=$(( $playerPosition - $dieValue ))
+			fi
+			;;
+		esac
+	done
 	echo "playerPosition " $playerPosition
 }
 checkOptions
+
+
