b0a2e4873d3b9ec072b4807880c5ed100b3ca47b
vegirevathi <vegirevathi@gmail.com>
2020-06-10 22:22:22 +0530

    ADDED: player options for No play, ladder and snake

--- Snakes_And_Ladders.sh
+++ Snakes_And_Ladders.sh
@@ -5,3 +5,25 @@ echo "Welcome to Snakes And Ladders"
 START_POSITION=0
 dieValue=$(( RANDOM%6 + 1 ))
 echo $dieValue
+
+function checkOptions()
+{
+	option=$(( RANDOM%3 + 1 ))
+	playerPosition=$START_POSITION	
+	noPlay=1
+	ladder=2
+	snake=3
+	case $option in
+	$noPlay)
+		playerPosition=$playerPosition
+		;;
+	$ladder)
+		playerPosition=$(( $playerPosition + $dieValue ))
+		;;
+	$snake)
+		playerPosition=$(( $playerPosition - $dieValue ))
+		;;
+	esac
+	echo "playerPosition " $playerPosition
+}
+checkOptions
